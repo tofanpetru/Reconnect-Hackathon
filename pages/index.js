d32d686f281ea6e0b233465d18ca5148e1639ded
bogusescu2000 <79723155+bogusescu2000@users.noreply.github.com>
2022-06-18 18:28:24 +0300

    Added layout

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        header
-      </Head>
-
       <main className={styles.main}>
        landing content
       </main>
-
-      <footer className={styles.footer}>
-       footer
-      </footer>
     </div>
   )
 }
